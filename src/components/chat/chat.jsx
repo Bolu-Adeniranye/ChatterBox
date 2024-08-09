a4c58e223f0bfe53d7d8108220d50b84d7c8683d
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-08-09 17:44:03 +0100

    Completed 2rd section with chat settings, chat display and stylings

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,38 +1,96 @@
 import EmojiPicker from "emoji-picker-react";
 import "./chat.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
   };
 
-  console.log(text);
   return (
     <div className="chat">
       <div className="top">
         <div className="user">
-          <img src="/avatar.png" alt="" />
+          <img src="./avatar.png" alt="" />
           <div className="texts">
             <span>Jane Doe</span>
             <p>Lorem ipsum dolor sit amet</p>
           </div>
         </div>
         <div className="icons">
-          <img src="/phone.png" alt="" />
-          <img src="/video.png" alt="" />
-          <img src="/info.png" alt="" />
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+              beatae, perferendis omnis ex unde deleniti qui inventore ab
+              eveniet eaque impedit possimus exercitationem suscipit ipsa
+              delectus animi voluptate, quis cumque.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+              beatae, perferendis omnis ex unde deleniti qui inventore ab
+              eveniet eaque impedit possimus exercitationem suscipit ipsa
+              delectus animi voluptate, quis cumque.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+              beatae, perferendis omnis ex unde deleniti qui inventore ab
+              eveniet eaque impedit possimus exercitationem suscipit ipsa
+              delectus animi voluptate, quis cumque.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              <img src="./bg.jpg" alt="" />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+              beatae, perferendis omnis ex unde deleniti qui inventore ab
+              eveniet eaque impedit possimus exercitationem suscipit ipsa
+              delectus animi voluptate, quis cumque.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+
       <div className="bottom">
         <div className="icons">
-          <img src="/img.png" alt="" />
-          <img src="/camera.png" alt="" />
-          <img src="/mic.png" alt="" />
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
         </div>
         <textarea
           type="text"
@@ -43,7 +101,7 @@ const Chat = () => {
         />
         <div className="emoji">
           <img
-            src="/emoji.png"
+            src="./emoji.png"
             alt=""
             onClick={() => setOpen((prev) => !prev)}
           />
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -8,18 +8,18 @@ const ChatList = () => {
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
-          <img src="/search.png" alt="" />
+          <img src="./search.png" alt="" />
           <input type="text" placeholder="Search" />
         </div>
         <img
-          src={addMode ? "/minus.png" : "/plus.png"}
+          src={addMode ? "./minus.png" : "./plus.png"}
           alt=""
           className="add"
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
       <div className="item">
-        <img src="/avatar.png" alt="" />
+        <img src="./avatar.png" alt="" />
         <div className="texts">
           <span>Jane Doe</span>
           <p>Hello</p>
@@ -27,7 +27,7 @@ const ChatList = () => {
       </div>
 
       <div className="item">
-        <img src="/avatar.png" alt="" />
+        <img src="./avatar.png" alt="" />
         <div className="texts">
           <span>Jane Doe</span>
           <p>Hello</p>
@@ -35,7 +35,7 @@ const ChatList = () => {
       </div>
 
       <div className="item">
-        <img src="/avatar.png" alt="" />
+        <img src="./avatar.png" alt="" />
         <div className="texts">
           <span>Jane Doe</span>
           <p>Hello</p>
@@ -43,7 +43,7 @@ const ChatList = () => {
       </div>
 
       <div className="item">
-        <img src="/avatar.png" alt="" />
+        <img src="./avatar.png" alt="" />
         <div className="texts">
           <span>Jane Doe</span>
           <p>Hello</p>
